6a3ff97493b6f3990ebdb6bc359780de207b9bc7
[Ignacio] <54185068+IgnacioAlvarado@users.noreply.github.com>
2022-05-04 14:57:36 -0500

    Delete actividad-archivos-iniciales directory

--- actividad-archivos-iniciales/routes/token.js
+++
@@ -1,7 +0,0 @@
-let express = require('express')
-let router = express.Router()
-let tokenController = require('../controllers/token')
-
-router.get('/confirmation/:token', tokenController.confirmationGet)
-
-module.exports = router
